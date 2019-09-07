6330e23be3f738e1fdd8a864c25f1c585e19a664
d0s1s <debiand@protonmail.com>
2019-09-07 14:38:01 +0200

    Revertidos últimos cambios hasta bb8ebd792e58e75889676099504c9c558a181ac4

--- Con_Acelerador_DAC_Millis_ProgNL_6kmh_1.8.ino
+++ Con_Acelerador_DAC_Millis_ProgNL_6kmh_1.8.ino
@@ -339,16 +339,19 @@ void freno() {
 }
 
 void ayudaArranque() {
+	// Guardamos valor de velocidad de crucero
+	float vcruceroprev = v_crucero;
+
 	// Mientras aceleramos y no pedaleamos
 	while (analogRead(pin_acelerador) > a0_min_value + 10 && p_pulsos == 0) {
+		v_crucero = sixkmh_acelerador; // Fijamos crucero a 6 km/h
 		contador_retardo_aceleracion++;
-		dac.setVoltage(aceleradorEnDac(sixkmh_acelerador),false);
+		mandaAcelerador();
+		delay(50); // Corrige duración del bucle de 30 sg si no se pone retardo
 	}
 
-	dac.setVoltage(aceleradorEnDac(0.85),false);
-	
-	// Cortamos crucero
-	v_crucero = voltaje_minimo;
+	// Recuperamos valor de velocidad de crucero
+	v_crucero = vcruceroprev;
 }
 
 void validaMinAcelerador() {
@@ -493,4 +496,4 @@ void loop() {
 	mandaAcelerador();
 }
 
-// Con_Acelerador_DAC_Millis_ProgNL_6kmh 1.7.1 Develop
+// Con_Acelerador_DAC_Millis_ProgNL_6kmh 1.8 Develop
