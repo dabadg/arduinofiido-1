fc241a28459374b3728755340304fc27d93acf0f
d0s1s <debiand@protonmail.com>
2019-10-26 14:47:32 +0200

    Refactor

--- Con_Acelerador_DAC_Millis_ProgNL_6kmh/config_sincrucero.h
+++ Con_Acelerador_DAC_Millis_ProgNL_6kmh/config_sincrucero.h
@@ -44,6 +44,11 @@ struct ConfigContainer {
 	// False --> Manda señal del acelerador.
 	boolean modo_crucero = false;
 
+	// True --> Habilita método de crucero contínuo. La variable
+	// pulsos_fijar_crucero no tiene efecto en este modo.
+	// False --> Habilita método de crucero por tiempo.
+	boolean modo_crucero_continuo = true;
+
 	// Cantidad de pasadas para fijar el crucero por tiempo.
 	// 20 * 140 = 2800 ms.
 	unsigned int pulsos_fijar_crucero = 20;
@@ -78,6 +83,10 @@ struct ConfigContainer {
 	// Tiempo de ejecución del progresivo en la asistencia a 6 km/h.
 	// 1500 ms.
 	int tiempo_ejecucion_progresivo_ayuda_arranque = 1500;
+	
+	// True --> El progresivo entra con crucero fijado o sin fijar.
+	// False --> El progresivo sólo entra si el crucero está fijado.
+	boolean modo_todo_progresivo = false;
 
 	// -------------- DEBUG
 	// Habilita la salida de datos por consola.
